1fcb127b43d5f572d4d60b81e9debed0c5ef4841
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-11-02 11:41:49 +0000

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,11 +34,10 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-
-
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
     @livewireStyles
+    @livewireScripts
     <style>
 
         .my-hover:hover
@@ -165,7 +164,7 @@
 
 
     <script src="{{ mix('js/app.js') }}"></script>
-    @livewireScripts
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     @stack('scripts')
