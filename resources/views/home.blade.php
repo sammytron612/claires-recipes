66628c31800114e5db67f7d840e29d38fd45b2c3
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-11-02 11:13:19 +0000

    ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
                       {{ session('error') }}
                     </div>
     @endif
-   {{-- @include('includes.search') --}}
+   @include('includes.search')
 
     <div class="mt-4">
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
