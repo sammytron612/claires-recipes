ec2106383fcd2d52dea8b50007724e2a18f9fe77
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-11-02 12:32:35 +0000

    ok

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
 <div class="container-fluid">
     <div class="bg-white row row-cols-1 row-cols-md-2 align-items-center py-2">
         <div class="d-flex justify-content-end col">
-            <img style="height:170px" class="img-fluid" src="{{ asset('storage/claires-recipe.png') }}">
+            <img style="height:170px" class="img-fluid" src="{{ asset('storage/claires-recipes.png') }}">
         </div>
         <div class="d-flex justify-content-start col">
             <h1 style="font-family: 'Pacifico', cursive;">{{ $title }}</h1>
