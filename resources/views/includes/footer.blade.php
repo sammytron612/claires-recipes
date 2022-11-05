50b2a00fe55ccac1cd1946bdb520a21a86f7698d
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-11-05 10:58:18 +0000

    ok

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,9 +3,9 @@
     <div class="container">
       <div class="row py-3">
         <div class="col-md-9">
-          <p class="text-white">© Kevin Wilson 2021</p>
+          <p class="text-white">© KLW-DESIGN 2021</p>
 
-            <a class="nav-link text-white" href="{{ url('home/privacy') }}">
+            <a class="nav-link text-white" href="{{ url('home/privacy') }}" aria-label="Privacy">
                 Privacy notice
             </a>
 
@@ -13,17 +13,17 @@
         <div class="col-md-3">
           <div class="d-inline-block">
             <div class="bg-circle-outline d-inline-block">
-              <a href="https://www.facebook.com/" class="text-white"><i class="fab fa-2x fa-facebook"></i>
+              <a href="https://www.facebook.com/" class="text-white"><i class="fab fa-2x fa-facebook" aria-label="Twitter"></i>
 		</a>
             </div>
 
             <div class="bg-circle-outline d-inline-block">
-              <a href="https://twitter.com/" class="text-white">
+              <a href="https://twitter.com/" class="text-white" aria-label="Twitter">
                 <i class="fab fa-2x fa-twitter"></i></a>
             </div>
 
             <div class="bg-circle-outline d-inline-block">
-              <a href="https://www.linkedin.com/company/" class="text-white">
+              <a href="https://www.linkedin.com/company/" class="text-white" aria-label="LinkedIn">
                 <i class="fab fa-2x fa-pinterest"></i></a>
             </div>
           </div>
