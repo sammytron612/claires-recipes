190dfa3d377026678a97258a696167c2f3effe9f
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-12-02 18:48:13 +0000

    ok

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -21,6 +21,6 @@ class isAdmin
             return $next($request);
        }
 
-       return redirect('home')->with('error','You have not admin access');
+       return redirect('home')->with('error','You have not got admin access');
     }
 }
