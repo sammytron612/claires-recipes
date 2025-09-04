4ea31a356d5fd52cc6486f273224e755e4d6d02c
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-09-04 11:00:28 +0100

    ok

--- resources/views/livewire/my-planner.blade.php
+++ resources/views/livewire/my-planner.blade.php
@@ -17,7 +17,7 @@
                 <h1 class="text-2xl font-bold text-gray-800">My Weekly Meal Planner</h1>
                 <p class="text-gray-600 mt-1">Plan your meals for the week ahead</p>
             </div>
-            <div class="mt-6 sm:mt-0">
+            <div class="mt-8 sm:mt-0">
                 <a href="{{ route('profile.shopping-list') }}" type="button" class="bg-teal-500 text-white px-4 py-2 rounded hover:bg-teal-600 transition">view my shopping list</a>
             </div>
         </div>
