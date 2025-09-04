637d846e3c9ee0099a4c3b36562ed26ec06920f8
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-09-04 09:59:36 +0100

    ok

--- resources/views/recipe-builder.blade.php
+++ resources/views/recipe-builder.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['title' => 'Recipe Builder | Claires Recipes', 'description' => "Search for Recipes on the ingredients you have."])
+@extends('layouts.app', ['noFollow' => false, 'title' => 'Recipe Builder | Claires Recipes', 'description' => "Search for Recipes on the ingredients you have."])
 
 @section('content')
 
