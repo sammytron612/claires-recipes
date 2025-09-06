d6c12deaf8cab099f652c07743901437eb96459d
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-09-06 18:35:00 +0100

    ok

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('profile.profile', absolute: false));
+        return redirect()->intended(route('home', absolute: false));
     }
 
     /**
