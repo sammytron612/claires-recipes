ab221d44378af6ed975dec98ce14147bb8d782a4
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-09-06 21:49:39 +0100

    ok

--- resources/views/blog/show-post.blade.php
+++ resources/views/blog/show-post.blade.php
@@ -51,7 +51,7 @@
     <div class="max-w-7xl mx-auto bg-white pb-5 px-4">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div class="text-center">
-                <img class="w-full rounded-lg shadow-md" src="{{$BlogArticle->main_image}}"/>
+                <img class="w-full rounded-lg shadow-md" src="{{url($BlogArticle->main_image)}}"/>
             </div>
             <div class="flex items-center">
                 <h1 style="font-family: 'Pacifico', cursive;" class="mx-auto mt-5 text-3xl md:text-4xl text-gray-800">{{$BlogArticle->title}}</h1>
