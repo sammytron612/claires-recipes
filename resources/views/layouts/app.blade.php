3fc8db60c2c2b942db458b7dfd01f343e3e5e5d7
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-09-06 19:46:42 +0100

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -133,12 +133,12 @@
                      
                         @guest
                             @if (Route::has('login'))
-                                <a class="text-teal-700 text-teal-800 hover:text-white px-2 sm:px-4 py-2 rounded-md hover:bg-teal-600 transition-all duration-200 font-medium text-sm" href="{{ route('login') }}" aria-label="Login">
+                                <a class="text-teal-700 text-teal-800 hover:text-white px-2 sm:px-4 py-2 rounded-md hover:bg-teal-600 transition-all duration-200 font-medium text-sm" href="{{ route('login') }}" aria-label="Login" wire:navigate>
                                     Login
                                 </a>
                             @endif
                             @if (Route::has('register'))
-                                <a class="bg-teal-600 text-white hover:text-teal-800 px-2 sm:px-4 py-2 rounded-md font-medium hover:bg-white transition-colors duration-200 text-sm whitespace-nowrap" href="{{ route('register') }}" aria-label="Register">
+                                <a class="bg-teal-600 text-white hover:text-teal-800 px-2 sm:px-4 py-2 rounded-md font-medium hover:bg-white transition-colors duration-200 text-sm whitespace-nowrap" href="{{ route('register') }}" aria-label="Register" wire:navigate>
                                     Sign Up
                                 </a>
                             @endif
