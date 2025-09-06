c6a2ee40b9a16f6a95b19bd779b0de49ac8196b4
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-09-06 22:01:19 +0100

    ok

--- resources/views/sitemap/blog.blade.php
+++ resources/views/sitemap/blog.blade.php
@@ -4,13 +4,30 @@
     @foreach($articles as $article)
     <url>
         <loc>{{ route('blog.show', ['id' => $article->id, 'slug' => $article->slug]) }}</loc>
-        <lastmod>{{ $article->updated_at->toISOString() }}</lastmod>
+        <lastmod>{{ $article->updated_at->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.7</priority>
+        
+        @if($article->main_image)
         <image:image>
-            <image:loc>{{ $article->main_image }}</image:loc>
+            @php
+                // Ensure full URL for main image
+                $imageUrl = $article->main_image;
+                if (!str_starts_with($imageUrl, 'http')) {
+                    // Handle storage URLs
+                    if (str_starts_with($imageUrl, 'storage/')) {
+                        $imageUrl = asset($imageUrl);
+                    } elseif (str_starts_with($imageUrl, '/storage/')) {
+                        $imageUrl = url($imageUrl);
+                    } else {
+                        $imageUrl = asset('storage/' . $imageUrl);
+                    }
+                }
+            @endphp
+            <image:loc>{{ $imageUrl }}</image:loc>
             <image:title>{{ $article->title }}</image:title>
         </image:image>
+        @endif
     </url>
     @endforeach
 </urlset>
