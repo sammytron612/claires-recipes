40b45b880749497034e627556ef851204fd48ad6
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-09-08 00:21:25 +0100

    ok

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -24,7 +24,7 @@ class AdminController extends Controller
     public function __construct(Request $request)
     {
 
-        dd(auth());
+        dd(auth()->user());
     }
     public function index()
     {
