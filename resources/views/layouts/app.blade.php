4c4e6c34d676ca9043dda89fbb6e4f81f030ad59
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-09-07 14:46:44 +0100

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,7 +66,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" rel="preload">
+    <link rel="dns-prefetch" href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" rel="preload">
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" rel="preload">
 
